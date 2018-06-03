9fad1207d9ca1578612348392dc7a2c3c3d25f63
Geoffrey Brossard <geoffrey@nangka.fr>
2018-06-03 20:03:42 +0200

    telegraf

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: ./kafka.sh
-worker: bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic telegraf && bin/telegraf -conf telegraf.conf
--- kafka.sh
+++ kafka.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 nohup kafka/bin/zookeeper-server-start.sh $ZOOKEEPER_CONF &
-kafka/bin/kafka-server-start.sh $KAFKA_CONF --override listeners="PLAINTEXT://:$PORT"
+kafka/bin/kafka-server-start.sh $KAFKA_CONF --override listeners="PLAINTEXT://:$PORT" &
+kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic telegraf
+bin/telegraf -conf telegraf.conf
+fg
