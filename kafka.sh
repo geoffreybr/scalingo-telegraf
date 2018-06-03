42a1a8886f8a0b6cd9be41c895fb98ba2d0d1eb6
Geoffrey Brossard <geoffrey@nangka.fr>
2018-06-03 19:42:00 +0200

    add topic creation after launch

--- kafka.sh
+++ kafka.sh
@@ -2,3 +2,4 @@
 
 nohup kafka/bin/zookeeper-server-start.sh $ZOOKEEPER_CONF &
 kafka/bin/kafka-server-start.sh $KAFKA_CONF --override listeners="PLAINTEXT://:$PORT"
+kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic influx
