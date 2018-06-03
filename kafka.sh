55808e1ee953ccdc3f73f4c1c8188a16c4329f2f
Geoffrey Brossard <geoffrey@nangka.fr>
2018-06-03 23:54:43 +0200

    fix telegraf

--- kafka.sh
+++ kafka.sh
@@ -3,5 +3,4 @@
 nohup kafka/bin/zookeeper-server-start.sh $ZOOKEEPER_CONF &
 kafka/bin/kafka-server-start.sh $KAFKA_CONF --override listeners="PLAINTEXT://:$PORT" &
 kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic telegraf
-bin/telegraf -conf telegraf.conf
-fg
+bin/telegraf --config telegraf.conf
