8aff28aca8d8ba077f112f388500a57523711174
Geoffrey Brossard <geoffrey@nangka.fr>
2018-06-04 00:02:47 +0200

    delay channel creation and telegraf registration

--- kafka.sh
+++ kafka.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 nohup kafka/bin/zookeeper-server-start.sh $ZOOKEEPER_CONF &
-kafka/bin/kafka-server-start.sh $KAFKA_CONF --override listeners="PLAINTEXT://:$PORT" &
-kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic telegraf
-bin/telegraf --config telegraf.conf
+
+sleep 20 && kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic telegraf &
+sleep 25 && bin/telegraf --config telegraf.conf &
+
+kafka/bin/kafka-server-start.sh $KAFKA_CONF --override listeners="PLAINTEXT://:$PORT"
